decf3faf00508d1ce2440a2af79d557ca861bd2f
tianxiaolong <tttx0307@163.com>
2022-09-30 15:12:37 +0800

    [设计模式] 模版模式

---
+++ JavaAndKt/com/location/java/design/template/StringDisplay.kt
@@ -0,0 +1,15 @@
+package com.location.java.design.template
+
+class StringDisplay: AbsDisplay() {
+    override fun open() {
+        println("open")
+    }
+
+    override fun print() {
+        println("print")
+    }
+
+    override fun close() {
+        println("close")
+    }
+}
